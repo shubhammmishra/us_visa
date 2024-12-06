aa18177e7e6c08f1f3b395952e6217d3413d3292
shubham <shubhammishra1442@gmail.com>
2024-12-06 22:32:16 +0530

    save

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='us_visa',
+    version='0.0.0',
+    author='shubham',
+    author_email='shubhammishra1442@gmail.com',
+    packages=find_packages()
+)
